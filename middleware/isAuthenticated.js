269fa73fbfeeae5e6f29368c0900bdfb4e1cee45
kaushalrijal <kaushalrijal091@gmail.com>
2024-07-17 02:04:45 +0545

    add authentication middleware

---
+++ middleware/isAuthenticated.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken")
+const User = require("../models/userModel")
+
+const isAuthenticated = (req, res, next) => {
+    const {token} = req.cookies
+    if(!token || token == null){
+        return res.status(400).json({
+            success: false,
+            message: "Not logged in"
+        })
+    }
+
+    jwt.verify(token, "meowmeow", async(error, result) => {
+        if(err){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Token"
+            })
+        }
+        const data = await User.findById(result.UserId);
+        if(!data){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid UID or Token"
+            })
+        }
+        req.userId = result.userId
+        next();
+    })
+}
